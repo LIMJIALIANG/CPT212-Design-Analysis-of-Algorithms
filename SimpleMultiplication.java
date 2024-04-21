ab5912534ca3e21e65e606bfb5d8dcd422ad5584
LJL <116794530+LIMJIALIANG@users.noreply.github.com>
2024-04-21 12:33:24 +0800

    Update SimpleMultiplication.java

--- SimpleMultiplication.java
+++ SimpleMultiplication.java
@@ -10,63 +10,45 @@ class SimpleMultiplication {
     public static long multiply(long x, long y) { // x = multiplicand, y = multiplier
         long result = 0;
         long multiplierPosition2 = 1; // not in the loop since it is used to multiply with the partial product and carrier from stage to stage
-        long lastDigitX, extraPartial, oldpartial = -1, oldcarrier = 0;
+        long lastDigitX, extraPartial;
 
-        while (y != 0) {
+        // Determine the number of digits in the multiplicand for formatting
+        int numDigits = String.valueOf(x).length();
+
+        for (int i = 0; i < numDigits; i++) {
             long resetX = x; // each time after ONE digit of multiplier multiplies with multiplicand, multiplicand will be reset
             long lastDigitY = y % 10; // last digit of multiplier, until multiplier turns "0", loop stops
             long multiplierPosition1 = 1;
-            long newpartial = 0;
-            long newcarrier = 0;
+            long partial = 0;
+            long carrier = 0;
 
-            // Determine the number of digits in the multiplicand for formatting
-            int numDigits = String.valueOf(x).length();
-
-            while (resetX != 0) {
+            for (int j = 0; j < numDigits; j++, resetX /= 10) {
                 lastDigitX = resetX % 10; // take the last digit of multiplicand everytime to multiply with last digit of multiplier
-                newpartial += lastDigitX * lastDigitY * multiplierPosition1;
+                partial += lastDigitX * lastDigitY * multiplierPosition1;
                 multiplierPosition1 *= 10; // multiply with 10 evertime partial product formed for the formation of carrier (if any)
 
-                if (newpartial >= multiplierPosition1) { // if carrier exist
-                    extraPartial = newpartial / multiplierPosition1;
-                    newcarrier += extraPartial * (multiplierPosition1 / 10);
-                    newpartial -= extraPartial * multiplierPosition1;
+                if (partial >= multiplierPosition1) { // if carrier exist
+                    extraPartial = partial / multiplierPosition1;
+                    carrier += extraPartial * (multiplierPosition1/10);
+                    partial -= extraPartial * multiplierPosition1;
                 }
-
-                resetX /= 10;
             }
 
-            // Format partial product and carrier to maintain leading zeros or ensure n digits of "0"
-            String partialProductStr = (newpartial == 0) ? String.format("%0" + numDigits + "d", 0) : String.format("%0" + numDigits + "d", newpartial);
-            String carrierStr = (newcarrier == 0) ? String.format("%0" + numDigits + "d", 0) : String.format("%0" + numDigits + "d", newcarrier);
-
-            System.out.println("Partial Product for (=" + x + " x " + lastDigitY + ") : " + partialProductStr);
-            System.out.println("Carrier for (=" + x + " x " + lastDigitY + ") : " + carrierStr);
-
-            y /= 10; // multiplier have new last digit for multiplication continuation
-
-            if (oldpartial == -1) { //use -1 to avoid logic error when oldpartial is 0 during multiplication process
-                result += newpartial;
-                oldpartial = newpartial;
-                oldcarrier = newcarrier;
-            } else {
-                result += oldcarrier * multiplierPosition2 + newpartial * multiplierPosition2;
-                oldpartial = newpartial;
-                oldcarrier = newcarrier;
-            }
+            partial *= multiplierPosition2;
+            carrier *= multiplierPosition2 * 10;
+            result += partial + carrier;
 
             multiplierPosition2 *= 10;
+            y /= 10; // multiplier have new last digit for multiplication continuation
 
-            if (y == 0) {
-                result += newcarrier * multiplierPosition2;
-            }
+            System.out.println("Partial Product for (=" + x + " x " + lastDigitY + ") : " + partial);
+            System.out.println("Carrier for (=" + x + " x " + lastDigitY + ") : " + carrier);
 
             // Increment counters
             assignmentCount += 2;
             multiplicationCount += 2;
             additionCount += 2;
         }
-
         return result;
     }
 
@@ -84,13 +66,15 @@ class SimpleMultiplication {
         additionCount = 0;
         multiplicationCount = 0;
 
+        // Display the Multiplicand and Multiplier calculated
+        System.out.println("Multiplicand: " + multiplicand);
+        System.out.println("Multiplier: " + multiplier + "\n");
+
         // Perform multiplication
         long result = multiply(multiplicand, multiplier);
 
         // Output the result and counters
-        System.out.println("\nMultiplicand: " + multiplicand);
-        System.out.println("Multiplier: " + multiplier);
-        System.out.println("Result: " + result);
+        System.out.println("\nResult: " + result);
         System.out.println("\nPrimitive Operations:");
         System.out.println("Assignments: " + assignmentCount);
         System.out.println("Additions: " + additionCount);
